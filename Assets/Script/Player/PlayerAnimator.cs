fc47a1a70549a92e6cc5960552cd95b423bf3925
bluet <fadityamputra@gmail.com>
2025-10-30 02:15:01 +0700

    Add `PlayerAnimator` to sync animations with movement direction
    - Created `PlayerAnimator` script to handle animation parameter updates based on movement direction.
    - Subscribed to `OnDirectionChanged` event in `PlayerMovement` for real-time animation updates.
    - Updated `PlayerMovement` to trigger `OnDirectionChanged` when direction changes.

---
+++ Assets/Script/Player/PlayerAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerMovement player;
+
+    private void OnEnable()
+    {
+        player.OnDirectionChanged += HandleDirectionState;
+    }
+
+    private void OnDisable()
+    {
+        player.OnDirectionChanged -= HandleDirectionState;
+    }
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        player = GetComponent<PlayerMovement>();
+    }
+
+    private void HandleDirectionState(Vector2 direction)
+    {
+        animator.SetInteger("MoveX", (int)direction.x);
+        animator.SetInteger("MoveY", (int)direction.y);
+    }
+}
